f30021e4e3693556569ec5102bc1078e4f1ffccb
Ralph Giles <rgiles@brave.com>
2021-09-24 09:38:06 -0700

    fixup: pass Owned(prng) as a pointer.

--- chromium_src/third_party/blink/renderer/core/execution_context/execution_context.cc
+++ chromium_src/third_party/blink/renderer/core/execution_context/execution_context.cc
@@ -141,7 +141,7 @@ AudioFarblingCallback BraveSessionCache::GetAudioFarblingCallback(
       }
       case BraveFarblingLevel::MAXIMUM: {
         uint64_t seed = *reinterpret_cast<uint64_t*>(domain_key_);
-        auto prng = std::mt19937_64(seed);
+        auto* prng = new std::mt19937_64(seed);
         return base::BindRepeating(&PseudoRandomSequence, base::Owned(prng),
                                    seed);
       }
